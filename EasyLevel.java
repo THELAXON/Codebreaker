4196f918368c5153ac3046c9324aa68f5f59735b
THELAXON <l.thavarajah@lancaster.ac.uk>
2022-03-12 22:59:49 +0000

    Changed game to easy level, each class is a level

---
+++ EasyLevel.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class EasyLevel implements ActionListener
+{
+    private JFrame frame = new JFrame("Code Breaker Game");
+    public EasyLevel()
+    {
+    
+
+        frame.setSize(400, 600);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(new Color(0x996600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
+ 
