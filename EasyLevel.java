7e067f7950f7e462e6da26e584f7c0d227fd7c61
THELAXON <l.thavarajah@lancaster.ac.uk>
2022-03-13 23:03:08 +0000

    Shortened the code further for imageicons

--- EasyLevel.java
+++ EasyLevel.java
@@ -8,29 +8,28 @@ import java.awt.event.ActionListener;
 public class EasyLevel implements ActionListener
 {
     //private static int codemaker[];
+    //private static int playerchoice[];
     private JFrame frame = new JFrame("Code Breaker Game");
     private JPanel panel = new JPanel();
     private static JButton buttons[] = new JButton[7];
     private static JLabel labels[] = new JLabel[6];
+    private static ImageIcon icons[] = new ImageIcon[10];
+    int j = 0, y=0;
     public EasyLevel()
     {
 
-        ImageIcon redicon = new ImageIcon("Colour_0.png");
-        ImageIcon orangeicon = new ImageIcon("Colour_1.png");
-        ImageIcon yellowicon = new ImageIcon("Colour_2.png");
-        ImageIcon greenicon = new ImageIcon("Colour_3.png");
-        ImageIcon blueicon = new ImageIcon("Colour_4.png");
-        ImageIcon indigoicon = new ImageIcon("Colour_5.png");
-        ImageIcon violeticon = new ImageIcon("Colour_6.png");
-        ImageIcon emptyicon = new ImageIcon("Empty.png");
-        ImageIcon score_0icon = new ImageIcon("Empty.png");
-        ImageIcon score_1icon = new ImageIcon("Empty.png");
+        for(int i = 0; i <=9;i++)
+        {
+            icons[i] = new ImageIcon(String.valueOf(j)+".png");
+            j++;
+        }
 
-        int j = 0, y=0;
         for(int i=0; i<=6;i++)
         {
             buttons[i] = new JButton();
-            buttons[i].setBounds(j,510,50,50);
+            buttons[i].setIcon(icons[i]);
+            buttons[i].setBounds(-10+j,510,50,50);
+            //buttons[i].setIcon(icons[i]);
             panel.add(buttons[i]);
             j = j + 50;
 
@@ -50,19 +49,12 @@ public class EasyLevel implements ActionListener
         frame.setLayout(null);
         frame.getContentPane().setBackground(new Color(0x996600)); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        buttons[0].setIcon(redicon);
-        buttons[1].setIcon(orangeicon);
-        buttons[2].setIcon(yellowicon);
-        buttons[3].setIcon(greenicon);
-        buttons[4].setIcon(blueicon);
-        buttons[5].setIcon(indigoicon);
-        buttons[6].setIcon(violeticon);
-        labels[0].setIcon(emptyicon);
+        /*labels[0].setIcon(emptyicon);
         labels[1].setIcon(emptyicon);
         labels[2].setIcon(emptyicon);
         labels[3].setIcon(emptyicon);
         labels[4].setIcon(score_0icon);
-        labels[5].setIcon(score_1icon);
+        labels[5].setIcon(score_1icon);*/
         frame.setVisible(true);
 
     }
@@ -77,7 +69,7 @@ public class EasyLevel implements ActionListener
 
     public static void redchange()
     {
-        ImageIcon redicon = new ImageIcon("Colour_0.png");
+        ImageIcon redicon = new ImageIcon("0.png");
         labels[0].setIcon(redicon);
     }
 
