0992a10a3c5a17f33593b50665324d868f02cfb8
THELAXON <l.thavarajah@lancaster.ac.uk>
2022-03-19 01:10:10 +0000

    playerchoices successfully compare with codemake

--- Codemaker.java
+++ Codemaker.java
@@ -1,35 +1,45 @@
 import java.util.Random;
+import java.util.Arrays; 
 
 public class Codemaker
 {
     private static int length = CodeBreaker.getarraylength();
-    static int[] codemake = new int[length];
+    //private static int[] playerlength = Level.getpc();
+    private static int[] codemake = new int[length];
     
     public Codemaker()
     {
         Random num = new Random();
-        for(int i=0;i<=length;i++)
+        for(int i=0;i<=length-1;i++)
         {
-            int x = num.nextInt(7);
+            int x = num.nextInt(6);
             codemake[i] = x;
         }
+        System.out.println(Arrays.toString(codemake));
     }
-    public static int[] getcodemake() {
+
+    public static int[] getcodemake() 
+    {
         return codemake;
     }
-    public static boolean win()
+    /*public static boolean win()
     {
-        for(int i=0;i<Level.getplayerchoice().length;i++)
-            {
+        if(playerlength== codemake)
+        {
+            return true;
+        }
+        for(int i=0;i<playerlength.length;i++)
+          {
                 for(int j=0;j<codemake.length;j++)
                 {
-                    if(Level.getplayerchoice()[i] == Codemaker.getcodemake()[j])
+                    if(Level.getpc()[i] == Codemaker.getcodemake()[j])
                     {
                         System.out.println("You won");
                         return true;
                     }
                 }
             }
-        return false;
-    }
+       return false;
+        
+    }*/
 }
--- Level.java
+++ Level.java
@@ -7,64 +7,64 @@ import java.util.Arrays;
 
 public class Level implements ActionListener
 {   
-    private static int length = CodeBreaker.getlength();
-    private static int rows = CodeBreaker.getrows();
-    private static int arraylength = CodeBreaker.getarraylength();
+    private static int length = CodeBreaker.getlength();                // Gets the length of guesses needed per row
+    private static int rows = CodeBreaker.getrows();                    // Gets the rows of guess needed per row
+    private static int arraylength = CodeBreaker.getarraylength();      // Decides what size the array should be
+    private static Codemaker hi = new Codemaker();
+    private static int[] computerchoice = Codemaker.getcodemake();                      // Uses the win class to decide if the player guesses correct
     //private static boolean win = Codemaker.win();
-    private static int[] computerchoice = Codemaker.getcodemake();
-    private JFrame frame = new JFrame("Code Breaker");                  // Naming the frame
+    private static JFrame frame = new JFrame("Code Breaker");                  // Naming the frame
     private static ImageIcon icons[] = new ImageIcon[10];               // Array to store image icons given
     private static JButton buttons[] = new JButton[7];                  // Button array to keep buttons seperately
-    private static JLabel labels[] = new JLabel[42];                    // JLabel size will change to adjust the rows and length
-    private static JLabel clabels[] = new JLabel[42];
+    private static JLabel labels[] = new JLabel[42];                    // Empty Label size will change to adjust the rows and length
+    private static JLabel clabels[] = new JLabel[42];                   // This is the checking labels array
     private static JPanel bpanel = new JPanel();                        // Button panel to hold buttons
-    private static JPanel rpanel = new JPanel();                        // Row panel to hold the empty icons
-    private static JPanel cpanel = new JPanel();
-    static int[] playerchoice = new int[arraylength];
-    
+    private static JPanel gpanel = new JPanel();                        // Row panel to hold the empty icons
+    private static JPanel cpanel = new JPanel();                        // Checking panel holds empty icons
+    private static int[] pc = new int[arraylength];                     // This is the player choices which will keep resetting for every row
     
     public Level()
     {
-        for(int i=0; i <=9;i++)                                         //Adding images to the icon array
+        for(int i=0; i <=9;i++)                                         // Adding images to the icon array
         {
-            icons[i] = new ImageIcon(String.valueOf(i)+".png");  // Converts to string the number used to name file and stores it into array
+            icons[i] = new ImageIcon(String.valueOf(i)+".png");         // Converts to string the number used to name file and stores it into array
         }
         
-        for(int i=0;i<=(length*rows)-1;i++)
+        for(int i=0;i<=(length*rows)-1;i++)                             // Creates enough empty labels for the grid layout      
         {
-            clabels[i] = new JLabel();
-            clabels[i].setIcon(icons[7]);
-            cpanel.add(clabels[i]);
+            clabels[i] = new JLabel();                                  // Assigns a new jlabel into the array
+            clabels[i].setIcon(icons[7]);                               // sets the empty icon as the icon for the label
+            cpanel.add(clabels[i]);                                     // Adds the label to the checking panel
         }
 
-        for(int i=0;i<=(length*rows)-1;i++)
+        for(int i=0;i<=(length*rows)-1;i++)                             // Creates enough empty labels for the grid layout
         {
-            labels[i] = new JLabel();
-            labels[i].setIcon(icons[7]);
-            rpanel.add(labels[i]);
+            labels[i] = new JLabel();                                   // Assigns a new jlabel into the array
+            labels[i].setIcon(icons[7]);                                // sets the empty icon as the icon for the label
+            gpanel.add(labels[i]);                                      // Adds the label to the guess panel
         }
 
         
-        for(int i=0;i<=6;i++)
+        for(int i=0;i<=6;i++)                                           // Needs to loop 7 times to create 7 buttons
         {
-            buttons[i] = new JButton();
-            buttons[i].setIcon(icons[i]);
-            buttons[i].addActionListener(this);
-            bpanel.add(buttons[i]);
+            buttons[i] = new JButton();                                 // Assigns the button to the button array
+            buttons[i].setIcon(icons[i]);                               // Assigns the icon representing each button
+            buttons[i].addActionListener(this);                         // Adds action listener to the button so program can react to various inputs
+            bpanel.add(buttons[i]);                                     // Adds the button to the button panel
         }
 
 
-        cpanel.setLayout(new GridLayout(rows*2,length/2,0,0));
-        cpanel.setBounds(350,20,130,450);
-        cpanel.setBackground(new Color(0x996600));
-        bpanel.setLayout(new GridLayout(1,7,0,0));
-        bpanel.setBounds(60,510,350,50);
-        bpanel.setBackground(new Color(0x996600));
-        rpanel.setLayout(new GridLayout(rows,length,0,30));
-        rpanel.setBounds(0,20,350,450);
-        rpanel.setBackground(new Color(0x996600));
+        cpanel.setLayout(new GridLayout(rows*2,length/2,0,0));          // Using grid layout for checking panel for better space usage
+        cpanel.setBounds(350,20,130,450);                               // Giving bounds for the panel so checks take place in that area of the frame
+        cpanel.setBackground(new Color(0x996600));                      // Background colour has to match the other backgrounds in the frame for consistency
+        bpanel.setLayout(new GridLayout(1,7,0,0));                      // Using grid layout for the button panel
+        bpanel.setBounds(60,510,350,50);                                // Giving set bounds to the bottom of the frame
+        bpanel.setBackground(new Color(0x996600));                      // Background colour has to match the other backgrounds in the frame for consistency
+        gpanel.setLayout(new GridLayout(rows,length,0,30));             // Grid layout for the game panel
+        gpanel.setBounds(0,20,350,450);                                 
+        gpanel.setBackground(new Color(0x996600));
         frame.add(bpanel);
-        frame.add(rpanel);
+        frame.add(gpanel);
         frame.add(cpanel);
         frame.setSize(500, 600);
         frame.setResizable(false);
@@ -72,13 +72,12 @@ public class Level implements ActionListener
         frame.getContentPane().setBackground(new Color(0x996600)); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
-
     }
     int p=0,k=0,j=0;
     @Override
     public void actionPerformed(ActionEvent e) 
     {
+        //System.out.println("This is the code"+Arrays.toString(computerchoice));
         for(int i=0;i<=6;i++)
         {
             if(e.getSource()== buttons[i])                                         
@@ -93,28 +92,39 @@ public class Level implements ActionListener
         }
                 if(j==arraylength)
                 {
-                    boolean win = Codemaker.win();
-                    if(win == true)
+                    //System.out.println(Arrays.toString(computerchoice));
+                    for(int i=0;i<pc.length;i++)
                     {
-                        frame.setVisible(false);
-                        new Winners();
+                        for(int j=0;j<computerchoice.length;j++)
+                        {
+                            if(pc[i] == computerchoice[j])
+                            {
+                                System.out.println("You won");
+                            }
+                        }
                     }
-                    //create instance of win class here
+                    //if(win == true)
+                    //{
+                    //   frame.setVisible(false);
+                    //    new Winners();
+                    //}
                     for(int y=0;y<arraylength;y++){
-                        playerchoice[y] = 0;
+                        pc[y] = 0;
                     }
                     j=0;
                 }
-                playerchoice[j]=k;
+                pc[j]=k;
                 j++;
-                System.out.println(Arrays.toString(playerchoice));
-}
+                System.out.println(Arrays.toString(pc));
+    }
 
-    public static int getarraylength() {
+    public static int getarraylength() 
+    {
         return arraylength;
     }
-    public static int[] getplayerchoice() {
-        return playerchoice;
+    public static int[] getpc() 
+    {
+        return pc;
     }
 }
  
